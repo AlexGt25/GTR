436e13dfc1a67d4c9a59f13ea16ce4ee37258078
Alex <gt25@live.com>
2015-12-10 00:55:04 +0300

    add Framework

---
+++ Framework/Framework/Converters.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace GTDev.Framework
+{
+
+    public class BooleanConverter<T> : IValueConverter
+    {
+        public BooleanConverter(T trueValue, T falseValue)
+        {
+            True = trueValue;
+            False = falseValue;
+        }
+
+        public T True { get; set; }
+        public T False { get; set; }
+
+        public virtual object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value is bool && ((bool)value) ? True : False;
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value is T && EqualityComparer<T>.Default.Equals((T)value, True);
+        }
+    }
+
+    public sealed class BooleanToVisibilityConverter : BooleanConverter<Visibility>
+    {
+        public BooleanToVisibilityConverter() :
+            base(Visibility.Visible, Visibility.Collapsed)
+        { }
+    }
+
+    public sealed class BooleanToRedGreenConverter : BooleanConverter<string>
+    {
+        public BooleanToRedGreenConverter() :
+            base("#FF00FF00", "#FFFF0000")
+        { }
+    }
+
+    public sealed class BooleanInverseConverter : BooleanConverter<bool>
+    {
+        public BooleanInverseConverter() :
+            base(false, true)
+        { }
+    }
+
+    public sealed class ToLocalTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Convert(object value, Type targetType, object parameter,
+       System.Globalization.CultureInfo culture)
+        {
+            return ((DateTime)value).ToLocalTime();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
